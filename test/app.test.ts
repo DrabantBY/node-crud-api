3e29b6d5e1068e0c2a652029679de9554f399eb7
DrabantBY <drabant2022@gmail.com>
2023-06-29 00:01:42 +0200

    feat: add all test cases

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,24 +1,40 @@
 import request from 'supertest';
-
 import { server } from '../src';
+import 'dotenv/config';
 
 const app = request(server);
+
+const changeErrorData = (
+  statusCode: number,
+  errorCode: number,
+  body: { message: string },
+  errorMessage: string
+) => {
+  expect(statusCode).toBe(errorCode);
+  expect(body.message).toBe(errorMessage);
+};
+
+let userId: string;
+const user = { username: 'Eugene', age: 35, hobbies: ['js', 'ts'] };
+const data = { username: 'Simon', age: 25, hobbies: ['soccer'] };
+const invalidUser = { username: 'Alex', age: 45, status: 'developer' };
+const invalidData = { username: 'Michael', age: 0 };
+
+const invalidJSON = '{ "username" "John" age: 30 }';
 
 afterAll(() => {
   server.close();
 });
 
-describe('get success responses', () => {
-  it('return an empty array with a GET request', async () => {
+describe('return success responses', () => {
+  it('return an empty array by GET request', async () => {
     const { statusCode, body } = await app.get('/api/users');
+
     expect(statusCode).toBe(200);
     expect(body).toHaveLength(0);
   });
 
-  let userId: string;
-
-  it('return new user with a POST request', async () => {
-    const user = { username: 'Eugene', age: 35, hobbies: ['js', 'ts'] };
+  it('return new user by POST request', async () => {
     const { statusCode, body } = await app.post('/api/users').send(user);
 
     expect(statusCode).toBe(201);
@@ -30,7 +46,7 @@ describe('get success responses', () => {
     userId = body.id;
   });
 
-  it('return new user with a GET request by id', async () => {
+  it('return new user by GET request by id', async () => {
     const { statusCode, body } = await app.get(`/api/users/${userId}`);
 
     expect(statusCode).toBe(200);
@@ -40,7 +56,7 @@ describe('get success responses', () => {
     expect(body.hobbies).toEqual(['js', 'ts']);
   });
 
-  it('return users array with a GET request', async () => {
+  it('return users array by GET request', async () => {
     const { statusCode, body } = await app.get('/api/users');
 
     expect(statusCode).toBe(200);
@@ -48,5 +64,198 @@ describe('get success responses', () => {
     expect(body[0].username).toBe('Eugene');
     expect(body[0].age).toBe(35);
     expect(body[0].hobbies).toEqual(['js', 'ts']);
+  });
+
+  it('update user by PUT request', async () => {
+    const { statusCode, body } = await app
+      .put(`/api/users/${userId}`)
+      .send(data);
+
+    expect(statusCode).toBe(200);
+    expect(body.username).toBe('Simon');
+    expect(body.age).toBe(25);
+    expect(body.hobbies).toHaveLength(1);
+    expect(body.hobbies).toEqual(['soccer']);
+  });
+
+  it('return updated user by GET request by id', async () => {
+    const { statusCode, body } = await app.get(`/api/users/${userId}`);
+
+    expect(statusCode).toBe(200);
+    expect(body.username).toBe('Simon');
+    expect(body.age).toBe(25);
+    expect(body.hobbies).toHaveLength(1);
+    expect(body.hobbies).toEqual(['soccer']);
+  });
+
+  it('return updated users array by GET request', async () => {
+    const { statusCode, body } = await app.get('/api/users');
+
+    expect(statusCode).toBe(200);
+    expect(body).toHaveLength(1);
+    expect(body[0].username).toBe('Simon');
+    expect(body[0].age).toBe(25);
+    expect(body[0].hobbies).toEqual(['soccer']);
+  });
+
+  it('remove user by DELETE request', async () => {
+    const { statusCode, body } = await app.delete(`/api/users/${userId}`);
+
+    expect(statusCode).toBe(204);
+    expect(body).toBeUndefined;
+  });
+
+  it('return an empty array by GET request', async () => {
+    const { statusCode, body } = await app.get('/api/users');
+
+    expect(statusCode).toBe(200);
+    expect(body).toHaveLength(0);
+  });
+});
+
+describe('return error 404 by invalid url', () => {
+  it('return error by GET request', async () => {
+    const { statusCode, body } = await app.get('/invalid_url/');
+
+    changeErrorData(
+      statusCode,
+      404,
+      body,
+      process.env.PATH_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error by POST request', async () => {
+    const { statusCode, body } = await app.post('/invalid_url/').send(user);
+
+    changeErrorData(
+      statusCode,
+      404,
+      body,
+      process.env.PATH_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error by PUT request', async () => {
+    const { statusCode, body } = await app.put('/invalid_url/').send(data);
+
+    changeErrorData(
+      statusCode,
+      404,
+      body,
+      process.env.PATH_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error by DELETE request', async () => {
+    const { statusCode, body } = await app.delete('/invalid_url/');
+
+    changeErrorData(
+      statusCode,
+      404,
+      body,
+      process.env.PATH_ERROR_MESSAGE ?? ''
+    );
+  });
+});
+
+describe('return error 400 by invalid id', () => {
+  it('return error by GET request', async () => {
+    const { statusCode, body } = await app.get('/api/users/invalid_uuid');
+
+    changeErrorData(
+      statusCode,
+      400,
+      body,
+      process.env.USER_ID_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error by PUT request', async () => {
+    const { statusCode, body } = await app
+      .put('/api/users/invalid_uuid')
+      .send(data);
+
+    changeErrorData(
+      statusCode,
+      400,
+      body,
+      process.env.USER_ID_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error by DELETE request', async () => {
+    const { statusCode, body } = await app.delete('/api/users/invalid_uuid');
+
+    changeErrorData(
+      statusCode,
+      400,
+      body,
+      process.env.USER_ID_ERROR_MESSAGE ?? ''
+    );
+  });
+});
+
+describe('return error 400 by invalid request body', () => {
+  it('return error by POST request', async () => {
+    const { statusCode, body } = await app
+      .post('/api/users/')
+      .send(invalidUser);
+
+    changeErrorData(
+      statusCode,
+      400,
+      body,
+      process.env.BODY_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error by PUT request', async () => {
+    const response = await app.post('/api/users/').send(user);
+
+    userId = response.body.id;
+
+    const { statusCode, body } = await app
+      .put(`/api/users/${userId}`)
+      .send(invalidData);
+
+    changeErrorData(
+      statusCode,
+      400,
+      body,
+      process.env.BODY_ERROR_MESSAGE ?? ''
+    );
+  });
+});
+
+describe('return internal error', () => {
+  it('return error 500 by POST request', async () => {
+    const { statusCode, body } = await app
+      .post('/api/users/')
+      .send(invalidJSON);
+
+    changeErrorData(
+      statusCode,
+      500,
+      body,
+      process.env.SERVER_ERROR_MESSAGE ?? ''
+    );
+  });
+
+  it('return error 500 by PUT request', async () => {
+    const response = await app.post('/api/users/').send(user);
+
+    userId = response.body.id;
+
+    const { statusCode, body } = await app
+      .put(`/api/users/${userId}`)
+      .send(invalidJSON);
+
+    changeErrorData(
+      statusCode,
+      500,
+      body,
+      process.env.SERVER_ERROR_MESSAGE ?? ''
+    );
   });
 });
